512a14f2c8356435e78a0459ef66029b0894c24d
Nguyen Tai <nguyentai21.fit@gmail.com>
2022-08-24 10:06:42 +0700

    update validate authen

--- src/api/driver/driver.route.ts
+++ src/api/driver/driver.route.ts
@@ -1,27 +1,29 @@
 import { Router } from "express";
+import { validateToken } from "../../middlewares/authen";
 // import passport from "passport";
 import driverController from "./driver.controller";
 
 const router = Router();
+const authenMiddleware = validateToken("USER");
 
 // const jwt_auth = passport.authenticate("jwt", { session: false });
 
-router.get("/drivers", driverController.get);
-router.get("/drivers/:id", driverController.getById);
+router.get("/drivers", authenMiddleware, driverController.get);
+router.get("/drivers/:id", authenMiddleware, driverController.getById);
 router.get(
   "/drivers/phone/:phone",
-
+  authenMiddleware,
   driverController.getByPhone
 );
 
-router.post("/drivers", driverController.post);
-router.post("/drivers/booking", driverController.booking);
+router.post("/drivers", authenMiddleware, driverController.post);
+router.post("/drivers/booking", authenMiddleware, driverController.booking);
 
-router.put("/drivers/accept", driverController.acceptBooking);
-router.put("/drivers/done", driverController.doneBooking);
-router.put("/drivers/cancel", driverController.cancelBooking);
-router.put("/drivers/:id", driverController.put);
+router.put("/drivers/accept", authenMiddleware, driverController.acceptBooking);
+router.put("/drivers/done", authenMiddleware, driverController.doneBooking);
+router.put("/drivers/cancel", authenMiddleware, driverController.cancelBooking);
+router.put("/drivers/:id", authenMiddleware, driverController.put);
 
-router.delete("/drivers/:id", driverController.delete);
+router.delete("/drivers/:id", authenMiddleware, driverController.delete);
 
 module.exports = router;
